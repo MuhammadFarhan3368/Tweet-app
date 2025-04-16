b9858cc35ccebcc117c9acc180fab381c7b5e9f1
Muhammad Farhan <muhammadfarhannajam@gmail.com>
2025-04-16 17:57:55 +0500

    try-catch with database modification

--- api/index.py
+++ api/index.py
@@ -1,10 +1,13 @@
-from django.core.wsgi import get_wsgi_application
 import sys
 import os
 
-# Setup environment
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tweet-app.settings")
+try:
+    from django.core.wsgi import get_wsgi_application
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweet-app.settings")
 
-app = get_wsgi_application()
-# This is the WSGI configuration for your Django project.
+    app = get_wsgi_application()
+
+except Exception as e:
+    print("ERROR:", str(e))  # This will show in Vercel logs
+    raise
